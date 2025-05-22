cc8e4873ca737b167e3d5d01be89b18d349dbb02
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2025-05-22 19:29:33 +0100

    created four new functions, refactored my createBlock function, the aim now is to make this concurrent to speed it up from 5m 11seconds.
    I have been working on the "block" part of my blockchain project for a few hours today, I have wrote the following functions:
    
    hashBlock: this function gets all the data about a block and hashes this data.
    
    timestamp: this function tells you the time in Ireland/Dublin.
    
    addBlockToChain: this function adds a block to the previous block that was made.
    
    createGenesisBlock: this function creates the first block for the blockchain.
    
    createBlock: I have refactored my createBlock function to now only take in an amount of blocks to create, I was previously passing in a block but I am not doing that anymore.
    
    and at the end of this I am calling all of my functions within my main method.
    
    below is the time it took to create 100 blocks:
    time it took to execute sequentially: 5m11.2780988s. The aim now is to make it concurrent with the use of Go-routines, wait-groups and much more.

--- Blocks/block.go
+++ Blocks/block.go
@@ -1,43 +1,113 @@
 package main
 
 import (
+	"BlockChainProjectFromScratch/pow"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"time"
-	"BlockChainProjectFromScratch/pow"
 )
 
 type Block struct {
 	index int
-	timestamp string
+	timestamp string 
 	prevHash string
 	transactions []int
-	proofOfWork bool
-	blockHash string
+	proofOfWork bool 
+	blockHash string 
 }
 
-func createBlock(block *Block) []Block {
-	
-	block.index = 1
-	
+func hashBlock(block Block) string {
+	data := fmt.Sprintf("%d%s%v%s%d", block.index, block.timestamp, block.transactions, block.prevHash, block.proofOfWork)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
+
+func timestamp() string {
 	location, _ := time.LoadLocation("Europe/Dublin")
-    currentTime := time.Now().In(location)
-    timeAndDate := currentTime.Format("2006-01-02 15:04:05")        
-	block.timestamp = timeAndDate
+	currentTime := time.Now().In(location)
+	timeAndDate := currentTime.Format("2006-01-02 15:04:05")   
+	return timeAndDate
+}
 
-	block.prevHash = "00000000000000000000000000000"
+func addBlockToChain(chain []Block) []Block {
+	lastBlock := chain[len(chain)-1]
+
+	newBlock := Block{}
+
+	newBlock.index = lastBlock.index + 1
+	time.Sleep(time.Second) // I use time.Sleep to simulate a block being made
+	newBlock.timestamp = timestamp()
+	newBlock.prevHash = lastBlock.prevHash
+	newBlock.transactions = lastBlock.transactions
+	newBlock.proofOfWork = pow.ProofOfWork("0000abc", "0000")
+	newBlock.blockHash = hashBlock(newBlock)
 	
-	trans := []int{1,2,3,4,5,6}
-	block.transactions = trans
+	chain = append(chain, newBlock)
+	return chain
+}
 
-	block.proofOfWork = pow.ProofOfWork("0000abc", "0000")
+func createGenesisBlock() []Block {
+	var chain []Block
+	genesis := Block{}
 
-	block.blockHash = "999999999999999999999999999999999"
+	genesis.index = 0
+	time.Sleep(time.Second) // I use time.Sleep to simulate a block being made
+	genesis.timestamp = timestamp()
+	genesis.prevHash = "00000000000000000000000000000"
+	genesis.transactions = []int{}
+	genesis.proofOfWork = false
+	genesis.blockHash = hashBlock(genesis)
 
+	chain = append(chain, genesis)
+	return chain
+}
+
+func createBlock(n int) []Block {
 	blockData := []Block{}
-	blockData = append(blockData, *block)
-	return blockData
+
+	for i:=0; i<n; i++ {
+		block := Block{}
+		
+		block.index = i + 1
+		time.Sleep(time.Second) // I use time.Sleep to simulate a block being made
+		block.timestamp = timestamp()
+
+		block.prevHash = "00000000000000000000000000000"
+		block.transactions = []int{1,2,3,4,5,6}
+
+		block.proofOfWork = pow.ProofOfWork("0000abc", "0000")
+		block.blockHash = hashBlock(block)
+
+		blockData = append(blockData, block)
+	}
+	return blockData 
 }
 
 func main() {
-	fmt.Println("this is a block:", createBlock(&Block{}))
+	now := time.Now()
+	defer func() {
+		fmt.Println("time it took to execute sequentially:", time.Since(now))
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Genesis Block:", createGenesisBlock(), "\n")
+
+	n := 100
+	time.Sleep(2 * time.Second)
+	block := createBlock(n)
+	
+	for _, n := range block {
+		time.Sleep(2 * time.Second)
+		fmt.Println("Constructing Block")
+		fmt.Println(n)
+	}
+
+	time.Sleep(5 * time.Second)
+	addedToChain := addBlockToChain(block)
+
+	for _, n := range addedToChain {
+		fmt.Println("\nBlock Added:", n, "\n\n")
+	}
+	fmt.Println("this is the total blockchain:", addedToChain)
 }
